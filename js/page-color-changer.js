969ce68ba44e027ec2b267f7786ae5522b7f7fc2
eldritch-quinn <quinn.faulkner02@gmail.com>
2024-02-07 11:48:41 -0500

    Added some comments and other various  intuitive fixes

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -1,17 +1,18 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  // Select the input elements in the document
+document.addEventListener("DOMContentLoaded", () => {
+  // Capture the RGB input elements in the document
   const r = document.querySelector("#red");
   const g = document.querySelector("#green");
   const b = document.querySelector("#blue");
 
-  // Define a new function
+  // Define a new function for the inputs
   function updateBackground() {
     // Set the background color of the document's body to the values of the inputs
     document.body.style.backgroundColor = `rgb(${r.value},${g.value},${b.value})`; // example : rgb(255,255,255)
   }
 
-  // Fire the function whenever any of the input's change
-  r.addEventListener("change", updateBackground);
-  b.addEventListener("change", updateBackground);
-  g.addEventListener("change", updateBackground);
-});
+  // Attach a listener to each input to update the background-color on every change.
+  for (const input of document.querySelectorAll("input"))
+    input.addEventListener("change", updateBackground);
+
+  // End of Code
+}); // https://github.com/eldritch-quinn/comp-1073-lab-1
